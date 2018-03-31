197ab0ce689b14bcae14c957287202ff75aa88f5
Sean Conrad <seanconrad123@gmail.com>
2018-03-31 00:54:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ function updateObjectWithKeyAndValue(object, key, value){
     p(object)
 }
 
+updateObjectWithKeyAndValue(recipes,"Burrito", "1 Tortilla")
+
 /*
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, obj, { [key]: value })
