2cae4fd3d869d227202721bfb957a0b07f3d0db1
Sean Conrad <seanconrad123@gmail.com>
2018-03-31 01:00:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,8 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
    return Object.assign(object, { [key]: value })
 }
+
+function deleteFromObjectByKey(object, key){
+  delete object.key
+  return object
+}
