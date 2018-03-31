3b15dcdafcaaee78cad3327ff492ca7529f97c6f
Sean Conrad <seanconrad123@gmail.com>
2018-03-31 00:49:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var recipes = { EggSandwhich: "2 eggs"}
  
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
-  return object
+  return recipes
 }
 
 p(updateObjectWithKeyAndValue(recipes, "EggSandwhich" , "1 Tortilla"))
