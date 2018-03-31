319d35ab7939b7fab3d5d437c7e4de30c690a3fe
Sean Conrad <seanconrad123@gmail.com>
2018-03-31 00:41:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ function p(value){
 var recipes = { EggSandwhich: "2 eggs"}
  
 function updateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
   return object
-  
+  return recipes
 }
 
 
