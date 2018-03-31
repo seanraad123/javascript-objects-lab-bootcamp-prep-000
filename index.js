d14f781a06200aa6044fbe400b60fb5f43fdf0f2
Sean Conrad <seanconrad123@gmail.com>
2018-03-31 00:57:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,12 +23,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
  
   return Object.assign({}, obj, { [key]: value })
 }
-  // it's important that we merge everything into
-  // a new object such as the empty {}. 
-    // Otherwise, the object obj will be modified. 
-    // Test what happens if this line was written as:
-    // return Object.assign(obj, { [key]: value })
- 
 const recipe = { eggs: 3 }
  
 updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
